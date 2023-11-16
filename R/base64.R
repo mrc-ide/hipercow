0b88e4ab1c8d6fc868ed58a408332edf068d6505
Rich FitzJohn <r.fitzjohn@imperial.ac.uk>
2023-11-16 15:02:02 +0000

    Import didehpc web client and tests

---
+++ R/base64.R
@@ -0,0 +1,74 @@
+decode64 <- function(x) {
+  storr_decode64(chartr("+/", "-_", x))
+}
+
+
+encode64 <- function(x) {
+  if (x == "") {
+    ""
+  } else {
+    storr_encode64(x, "+", "/")
+  }
+}
+
+
+storr_encode64 <- function(x, char62 = "-", char63 = "_", pad = TRUE) {
+  if (length(x) != 1L) {
+    return(vcapply(x, encode64, char62, char63, pad, USE.NAMES = FALSE))
+  }
+  tr <- c(LETTERS, letters, 0:9, char62, char63)
+  x <- as.integer(charToRaw(x))
+  n_bytes <- length(x)
+  n_blocks <- ceiling(n_bytes / 3L)
+  n_pad <- 3L * n_blocks - n_bytes
+
+  ## The integer() call here pads the *input* to have the correct number
+  ## of blocks of bytes.
+  x <- matrix(c(x, integer(3L * n_blocks - n_bytes)), 3L, n_blocks)
+
+  y <- matrix(integer(4 * n_blocks), 4L, n_blocks)
+  y[1L, ] <- bitwShiftR(x[1L, ], 2L)
+  y[2L, ] <- bitwOr(bitwShiftL(x[1L, ], 4L), bitwShiftR(x[2L, ], 4L))
+  y[3L, ] <- bitwOr(bitwShiftL(x[2L, ], 2L), bitwShiftR(x[3L, ], 6L))
+  y[4L, ] <- x[3L, ]
+
+  z <- tr[bitwAnd(y, 63L) + 1L]
+  if (n_pad > 0) {
+    len <- length(z)
+    z[(len - n_pad + 1):len] <- if (pad) "=" else ""
+  }
+  paste0(z, collapse = "")
+}
+
+storr_decode64 <- function(x, char62 = "-", char63 = "_", error = TRUE) {
+  if (length(x) != 1L) {
+    return(vcapply(x, decode64, char62, char63, error, USE.NAMES = FALSE))
+  }
+  if (FALSE && !grepl("^[A-Za-z0-9_-]*=*$", x)) {
+    if (error) {
+      stop(sprintf("Input '%s' is not base64 (url) encoded", x))
+    } else {
+      return(NA_character_)
+    }
+  }
+
+  ## TODO: check that the string is correctly encoded before doing
+  ## anything.
+  tr <- c(LETTERS, letters, 0:9, char62, char63)
+
+  ## sub is the timesink here, followed by strsplit.  charToRaw might be better.
+  x <- strsplit(sub("=+$", "", x), NULL)[[1]]
+  y <- match(x, tr) - 1L
+
+  n_byte <- length(y)
+  n_block <- ceiling(n_byte / 4L)
+
+  y <- matrix(c(y, integer(4L * n_block - n_byte)), 4L, n_block)
+  x <- matrix(integer(3 * n_block), 3, n_block)
+  x[1L, ] <- bitwOr(bitwShiftL(y[1L, ], 2L), bitwShiftR(y[2L, ], 4L))
+  x[2L, ] <- bitwOr(bitwShiftL(y[2L, ], 4L), bitwShiftR(y[3L, ], 2L))
+  x[3L, ] <- bitwOr(bitwShiftL(y[3L, ], 6L), y[4L, ])
+  x <- bitwAnd(x, 255L)
+
+  rawToChar(as.raw(x))
+}
---
+++ R/util_assert.R
@@ -0,0 +1,28 @@
+assert_scalar <- function(x, name = deparse(substitute(x))) {
+  if (length(x) != 1) {
+    stop(sprintf("'%s' must be a scalar", name), call. = FALSE)
+  }
+}
+
+
+assert_scalar_character <- function(x, name = deparse(substitute(x))) {
+  assert_scalar(x, name)
+  assert_character(x, name)
+}
+
+
+assert_character <- function(x, name = deparse(substitute(x))) {
+  if (!is.character(x)) {
+    stop(sprintf("'%s' must be a character", name), call. = FALSE)
+  }
+}
+
+
+match_value <- function(arg, choices, name = deparse(substitute(arg))) {
+  assert_scalar_character(arg)
+  if (!(arg %in% choices)) {
+    stop(sprintf("'%s' must be one of %s",
+                 name, paste(squote(choices), collapse = ", ")))
+  }
+  arg
+}
