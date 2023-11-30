2fa9a9d3b661eb57c79131e7dd4fd23daaad0cde
Rich FitzJohn <r.fitzjohn@imperial.ac.uk>
2023-11-30 16:53:05 +0000

    Test that we know all arguments

--- tests/testthat/test-interface.R
+++ tests/testthat/test-interface.R
@@ -24,3 +24,17 @@ test_that("can submit a task via a driver", {
 
   expect_true(withr::with_dir(path_there, hermod_task_eval(id)))
 })
+
+
+test_that("forbid additional arguments to submission, for now", {
+  elsewhere_register()
+  path_here <- withr::local_tempdir()
+  path_there <- withr::local_tempdir()
+  init_quietly(path_here)
+  init_quietly(path_there)
+  hermod_configure("elsewhere", path = path_there, root = path_here)
+  id <- hermod_task_create_explicit(quote(getwd()), root = path_here)
+  expect_error(
+    withr::with_dir(path_here, hermod_task_submit(id, cores = 2)),
+    "Additional arguments to 'hermod_task_submit' not allowed")
+})
