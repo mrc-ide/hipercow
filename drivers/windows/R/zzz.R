a3f01955fc055cbc7088890943912b0a51ffeed3
Rich FitzJohn <r.fitzjohn@imperial.ac.uk>
2023-12-12 17:42:43 +0000

    Prevent package loading

--- drivers/windows/R/zzz.R
+++ drivers/windows/R/zzz.R
@@ -1,1 +1,11 @@
 cache <- new.env(parent = emptyenv())
+
+
+.onAttach <- function(...) {
+  # nocov start
+  msg <- paste("You have called 'library(hermod.windows)' but you don't need",
+               "to; this package just needs to be installed to work, and you",
+               "only need to call 'library(hermod)'")
+  packageStartupMessage(paste(strwrap(msg), collapse = "\n"))
+  # nocov end
+}
