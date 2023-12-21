61532247a019a198eb2104adf6a997753ac41f8d
Rich FitzJohn <r.fitzjohn@imperial.ac.uk>
2023-12-21 11:14:18 +0000

    Use logwatch for logs

--- drivers/windows/R/provision.R
+++ drivers/windows/R/provision.R
@@ -1,5 +1,6 @@
 ## windows-specific provisioning code, called from hipercow
-windows_provision <- function(method, config, path_root, environment, ...) {
+windows_provision <- function(method, config, path_root, environment, ...,
+                              show_log = TRUE, poll = 1) {
   conan_config <- conan2::conan_configure(
     method,
     path = path_root,
@@ -27,17 +28,29 @@ windows_provision <- function(method, config, path_root, environment, ...) {
 
   path_log <- file.path(dirname(path_batch), "log")
 
-  status <- function() {
+  get_status <- function() {
     switch(client$status_job(dide_id),
            "PENDING" = "waiting",
            "RUNNING" = "running",
            "COMPLETE" = "success",
            "failure") # "ERROR", "CANCELLED" or unknown status
   }
+  get_log <- function() {
+    readlines_if_exists(path_log, warn = FALSE)
+  }
 
-  conan2::conan_watch(
-    status,
-    function() readlines_if_exists(path_log, warn = FALSE),
-    show_log = conan_config$show_log,
-    poll = conan_config$poll)
+  res <- logwatch::logwatch(
+    "installation",
+    get_status,
+    get_log,
+    show_log = show_log,
+    poll = poll)
+  elapsed <- format(res$end - res$start, digits = 4)
+  if (res$status == "success") {
+    cli::cli_alert_success(
+      "Installation script finished successfully in {elapsed}")
+  } else {
+    cli::cli_abort("Installation failed after {elapsed}")
+  }
+  res
 }
