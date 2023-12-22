6574752f42c00ca47197499efda729e65aeed52d
Rich FitzJohn <r.fitzjohn@imperial.ac.uk>
2023-12-22 10:34:56 +0000

    Basic bulk submission support

---
+++ tests/testthat/test-task-bulk.R
@@ -0,0 +1,45 @@
+test_that("bulk creation of a task", {
+  path <- withr::local_tempdir()
+  init_quietly(path)
+
+  d <- data.frame(a = 1:3, b = c("x", "y", "z"))
+  x <- 1
+  id <- withr::with_dir(path, task_create_bulk_expr(list(x, a, b), d))
+
+  expect_length(id, 3)
+  expect_type(id, "character")
+  expect_match(id, "^[[:xdigit:]]{32}$", all = TRUE)
+  expect_equal(task_status(id, root = path), rep("created", 3))
+
+  d <- lapply(file.path(path, "hipercow", "tasks", id, "expr"), readRDS)
+  expect_equal(d[[1]]$variables$locals, list(x = 1, a = 1, b = "x"))
+  expect_equal(d[[2]]$variables$locals, list(x = 1, a = 2, b = "y"))
+  expect_equal(d[[3]]$variables$locals, list(x = 1, a = 3, b = "z"))
+
+  expect_equal(d[[1]]$expr, quote(list(x, a, b)))
+  expect_equal(d[[1]]$environment, "default")
+  expect_equal(d[[1]]$path, ".")
+  expect_equal(d[[1]]$type, "expression")
+
+  v <- c("type", "path", "environment", "expr")
+  expect_equal(d[[2]][v], d[[1]][v])
+  expect_equal(d[[3]][v], d[[1]][v])
+})
+
+
+test_that("use splicing to disambiguate expressions", {
+  path <- withr::local_tempdir()
+  init_quietly(path)
+
+  d <- data.frame(a = 1:3, b = c("x", "y", "z"))
+  a <- 1
+  id <- withr::with_dir(path, task_create_bulk_expr(list(!!a, a, b), d))
+
+  d <- lapply(file.path(path, "hipercow", "tasks", id, "expr"), readRDS)
+  expect_equal(d[[1]]$variables$locals, list(a = 1, b = "x"))
+  expect_equal(d[[2]]$variables$locals, list(a = 2, b = "y"))
+  expect_equal(d[[3]]$variables$locals, list(a = 3, b = "z"))
+  expect_equal(d[[1]]$expr, quote(list(1, a, b)))
+  expect_equal(d[[2]]$expr, quote(list(1, a, b)))
+  expect_equal(d[[3]]$expr, quote(list(1, a, b)))
+})
