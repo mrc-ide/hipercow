92e7ae66397a1a3f47c683b4086ff7e6427ebc40
Wes Hinsley <w.hinsley@imperial.ac.uk>
2024-01-19 19:17:10 +0000

    Better

--- R/parallel.R
+++ R/parallel.R
@@ -112,7 +112,7 @@ hipercow_parallel_setup_parallel <- function(cores) {
   cl <- parallel::makeCluster(spec = cores)
   parallel::setDefaultCluster(cl)
   # set lib paths to my lib paths
-  parallel::clusterCall(cl, function(x) .libPaths(x), .libPaths())
+  parallel::clusterCall(cl, .libPaths, .libPaths())
   parallel::clusterCall(cl, hipercow::hipercow_parallel_set_cores, 1)
   # may need some tweaking to find the function.
   # later on we'll also load some packages, source some files
