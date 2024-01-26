4d1fd674df98070fc89cb20210ce3347c6183757
Rich FitzJohn <r.fitzjohn@imperial.ac.uk>
2024-01-26 09:22:32 +0000

    Expand information on what is reported about cluster

--- drivers/windows/R/resource.R
+++ drivers/windows/R/resource.R
@@ -1,18 +1,24 @@
 ## windows-specific cluster functions, called from hipercow.
 
 windows_cluster_info <- function(config, path_root) {
-  if (config$cluster == "wpia-hn") {
-    info <- list(
-      max_cores = 32,
-      max_ram = 512,
-      queues = c("AllNodes", "Training"),
-      default_queue = "AllNodes",
-      nodes = sprintf("wpia-%003d", (1:70)[-c(41, 42, 49, 50)])
-    )
-    class(info) <- "windows_cluster_info"
-    return(info)
-  }
-  cli::cli_abort(c(
-    "Cluster {config$cluster} not supported by windows driver.",
-    i = "Use wpia-hn."))
+  switch(config$cluster,
+         "wpia-hn" = cluster_info_wpia_hn(),
+         cli::cli_abort(c(
+           "Cluster '{config$cluster}' not supported by windows driver",
+           i = "Use wpia-hn.")))
+}
+
+
+cluster_info_wpia_hn <- function() {
+  resources <- list(
+    max_cores = 32,
+    max_ram = 512,
+    queues = c("AllNodes", "Training"),
+    default_queue = "AllNodes",
+    nodes = sprintf("wpia-%003d", (1:70)[-c(41, 42, 49, 50)])
+  )
+  redis_url <- NULL # not ready to be used yet
+  list(resources = resources,
+       r_versions = r_versions(),
+       redis_url = redis_url)
 }
