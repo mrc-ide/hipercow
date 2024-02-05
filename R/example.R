dcec3ec549e0c56900d669fd43221460c08d2102
Wes Hinsley <w.hinsley@imperial.ac.uk>
2024-02-05 10:00:43 +0000

    When will rstudio remove whitespace for me?!

--- R/example.R
+++ R/example.R
@@ -198,7 +198,7 @@ example_check_hello <- function(config, path_root) {
 
 
 example_cluster_info <- function(config, path_root) {
-  resources <- list(max_ram = 4, max_cores = 4, 
+  resources <- list(max_ram = 4, max_cores = 4,
                     queues = c("alltasks", "bigmem", "fast"),
                     nodes = c("node-1", "node-2", "gpu-3", "gpu-4"),
                     default_queue = "alltasks")
