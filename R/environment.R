68262218c8ae2a6e70b2752d782e7cf248498454
Rich FitzJohn <r.fitzjohn@imperial.ac.uk>
2024-11-26 20:02:09 +0000

    Use function that already is merged

--- R/environment.R
+++ R/environment.R
@@ -102,7 +102,8 @@ hipercow_environment_create <- function(name = "default", packages = NULL,
   if (name == "rrq" && is_rrq_enabled(root)) {
     cli::cli_alert_info("Refreshing existing rrq worker environments")
     controller <- hipercow_rrq_controller(root)
-    rrq::rrq_worker_refresh(controller)
+    ## or rrq::rrq_worker_refresh(controller) in recent rrq
+    rrq_message_send("REFRESH", controller = controller)
   }
 }
 
