1287e6389be16b8929e7587cec6c9ad5f7287332
Wes Hinsley <w.hinsley@imperial.ac.uk>
2025-02-23 21:37:40 +0000

    Possible bootstrap fix

--- drivers/windows/inst/templates/bootstrap.R
+++ drivers/windows/inst/templates/bootstrap.R
@@ -11,7 +11,18 @@ if (file.exists(path_next)) {
   stop("Failed to remove previous-next library")
 }
 dir.create(path_next, FALSE, TRUE)
-.libPaths(path_next, FALSE)
+
+set_lib_paths <- function(lib_vec) {
+  lib_vec <- normalizePath(lib_vec, mustWork = TRUE)
+  shim_fun <- .libPaths
+  shim_env <- new.env(parent = environment(shim_fun))
+  shim_env$.Library <- character()
+  shim_env$.Library.site <- character()
+  environment(shim_fun) <- shim_env
+  shim_fun(lib_vec)
+}
+
+set_lib_paths(path_next)
 message(sprintf("Installing packages into %s", path_next))
 pkgs <- c("hipercow", "remotes", "pkgdepends", "renv", "rrq")
 repos <- c("https://mrc-ide.r-universe.dev", "https://cloud.r-project.org")
