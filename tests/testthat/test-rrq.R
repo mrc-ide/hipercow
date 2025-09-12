55d693eefafd8f6a5489fb3b84a1723e21ecc6ef
Rich FitzJohn <r.fitzjohn@imperial.ac.uk>
2025-09-12 11:11:31 +0100

    Fix tests

--- tests/testthat/test-rrq.R
+++ tests/testthat/test-rrq.R
@@ -97,6 +97,7 @@ test_that("can't submit workers from below root", {
 
 
 test_that("can load rrq environment by preference", {
+  skip_if_no_redis()
   path <- withr::local_tempdir()
   writeLines("f <- function() 1", file.path(path, "a.R"))
   writeLines("f <- function() 2", file.path(path, "b.R"))
