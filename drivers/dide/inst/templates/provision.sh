d5c1c246010fb9d85cf7a4248fd7f91b21b25194
Wes Hinsley <w.hinsley@imperial.ac.uk>
2025-10-18 08:28:53 +0100

    Don't mess with module

--- drivers/dide/inst/templates/provision.sh
+++ drivers/dide/inst/templates/provision.sh
@@ -5,10 +5,7 @@ echo generated on date: {{date}}
 echo hipercow version: {{hipercow_version}}
 echo running on: $(hostname -f)
 
-export PATH=/opt/apps/lmod/lmod/libexec:$PATH
-source /opt/apps/lmod/lmod/init/bash
-export LMOD_CMD=/opt/apps/lmod/lmod/libexec/lmod
-module use /modules-share/modules/all
+module use /modules/modules/all
 
 module load R/{{r_version}}
 module load hiredis
